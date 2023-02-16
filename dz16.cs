2c9c49a813a8ed6abba3b95cb733627a238b4b95
Egor_Not_Found <sittnikkovegorr77@mail.ru>
2023-02-16 16:29:40 +0300

    first commit

---
+++ dz16.cs
@@ -0,0 +1,30 @@
+﻿//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+double [] array = GetArray (12 ,-100 , 100);
+double difference =0;
+double min = 0;
+double max = 0;
+Console.WriteLine(String.Join(" ", array.Select(n=>$"{n:F2}")));
+double [] GetArray (int size, int minValue, int maxValue)
+{
+    double[] res = new double [size];
+    for (int i =0; i< size;i++ )
+    {
+        res[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return res;
+}
+for (int i = 0; i< array.Length;i++){
+   if(array[i]<min){
+    min = array[i];
+}
+if(array[i]>max){
+    max = array[i];
+}
+}
+difference = max - min;
+if(difference<0){
+    difference = difference*(-1);
+}
+
+Console.WriteLine("Минимальный элемент: " + min  + " Максимальный элемент: " + max +  " Разница между минимальным и максимальным элементом: " + difference );
+
